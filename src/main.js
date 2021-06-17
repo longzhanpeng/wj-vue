d0c707e83a62569d4bacacebb7ca83da32557c69
longzhanpeng <chinpang9527@163.com>
2021-06-17 15:58:33 +0800

    feat:登录逻辑熟悉

--- src/main.js
+++ src/main.js
@@ -3,19 +3,41 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 var axios = require('axios')
-axios.defaults.baseURL = 'http://localhsot:8443/api'
-
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
+axios.defaults.baseURL = 'http://localhost:8443/api'
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
-Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+// router.beforeEach()，意思是在访问每一个路由前调用
+router.beforeEach((to, from, next) => {
+  console.log('to:' + to)
+  if (to.meta.requireAuth) {
+    if (store.state.user.username) {
+      next()
+    } else {
+      next({
+        path: 'login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  } else {
+    next()
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  store,
+  components: {App},
   template: '<App/>'
 })
